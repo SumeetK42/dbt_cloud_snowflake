5b6b151bbb8eff4ee68090c059932bd9a1726f40
Sumeet karmankar <sumeetkarmankar42@gmail.com>
2025-12-16 15:16:02 +0000

    added staging data and then added the dimensions table on the customer for the schema and changed the materliation

---
+++ models/marts/dim_customer.sql
@@ -0,0 +1,27 @@
+with customers as (
+select * from {{ ref ('stg_jaffle_shop__customers') }}
+),
+orders as (
+   select * from {{ ref('stg_jaffle_shop__orders') }}
+),
+customer_orders as (
+    select  
+      customer_id,
+      max(order_date) as most_recent_order_date,
+      min(order_date) as first_order_date,
+      count(*) as total_orders
+    from orders
+    group by 1
+),
+final as (
+    select 
+     a.customer_id,
+     a.first_name,
+     a.last_name,
+     b.first_order_date,
+     b.most_recent_order_date,
+     coalesce(total_orders,0) as no_of_orders
+    from customers  a 
+    left join customer_orders b  on a.customer_id = b.customer_id
+)
+select * from final
