70a01f731c1c455a70ba2e48d6a30aa288d42e7f
Sumeet karmankar <sumeetkarmankar42@gmail.com>
2025-12-16 16:48:35 +0000

    facts dimension added to the model

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -2,14 +2,15 @@ with customers as (
 select * from {{ ref ('stg_jaffle_shop__customers') }}
 ),
 orders as (
-   select * from {{ ref('stg_jaffle_shop__orders') }}
+   select * from {{ ref('fct_orders') }}
 ),
 customer_orders as (
     select  
       customer_id,
       max(order_date) as most_recent_order_date,
       min(order_date) as first_order_date,
-      count(*) as total_orders
+      count(*) as total_orders,
+      sum(amount) as lifetime_value
     from orders
     group by 1
 ),
@@ -20,7 +21,8 @@ final as (
      a.last_name,
      b.first_order_date,
      b.most_recent_order_date,
-     coalesce(total_orders,0) as no_of_orders
+     coalesce(total_orders,0) as no_of_orders,
+     b.lifetime_value
     from customers  a 
     left join customer_orders b  on a.customer_id = b.customer_id
 )
